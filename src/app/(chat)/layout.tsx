3126deea196c7e3d099f4dca47decf050668fa59
Erlhon9 <hongsbus@gmail.com>
2024-07-27 02:17:06 +0900

    fix: remove input

--- src/app/(chat)/layout.tsx
+++ src/app/(chat)/layout.tsx
@@ -1,11 +1,10 @@
 import { Center } from '@chakra-ui/react';
-import { ChatInput, Navigation } from './components';
+import { Navigation } from './components';
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
     return (
         <Center as="main" minH="100dvh">
             {children}
-            <ChatInput galleryButtonProps={undefined} inputProps={undefined} sendButtonProps={undefined} />
             <Navigation />
         </Center>
     );
