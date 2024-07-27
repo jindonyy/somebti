b5680c96ea2d1306e5bbb80a79d85c9bca86ade9
jindonyy <rsefa94@gmail.com>
2024-07-28 07:42:50 +0900

    feat: 이미 로그인했을 시 로그인 페이지 접근 못하도록 수정

--- src/app/(auth)/login/components/KakaoLoginButton.tsx
+++ src/app/(auth)/login/components/KakaoLoginButton.tsx
@@ -2,9 +2,20 @@
 
 import { useKakaoLogin } from '@/hooks';
 import { BottomButton } from '@/components';
+import { useEffect } from 'react';
+import { getCookie } from 'cookies-next';
+import { useRouter } from 'next/navigation';
 
 export default function KakaoLoginButton() {
+    const router = useRouter();
     const { kakaoLogin } = useKakaoLogin();
+
+    useEffect(() => {
+        const token = getCookie('access_token');
+        if (token) {
+            router.replace('/');
+        }
+    });
 
     return <BottomButton onClick={kakaoLogin}>카카오 로그인하기</BottomButton>;
 }
