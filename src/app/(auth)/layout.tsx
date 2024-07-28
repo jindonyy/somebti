daf261531b83703948a593d0fc0a5a9fe314e747
jindonyy <rsefa94@gmail.com>
2024-07-28 11:02:08 +0900

    feat: auth 페이지들 로그인 상태에서 진입 시 라우팅 되도록 수정

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { getCookie } from 'cookies-next';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+type Props = Readonly<{
+    children: React.ReactNode;
+}>;
+
+export default function Layout(props: Props) {
+    const { children } = props;
+    const router = useRouter();
+
+    useEffect(() => {
+        const token = getCookie('access_token');
+        if (token) {
+            router.replace('/');
+        }
+    });
+
+    return <>{children}</>;
+}
--- src/app/(auth)/login/components/KakaoLoginButton.tsx
+++ src/app/(auth)/login/components/KakaoLoginButton.tsx
@@ -2,20 +2,9 @@
 
 import { useKakaoLogin } from '@/hooks';
 import { BottomButton } from '@/components';
-import { useEffect } from 'react';
-import { getCookie } from 'cookies-next';
-import { useRouter } from 'next/navigation';
 
 export default function KakaoLoginButton() {
-    const router = useRouter();
     const { kakaoLogin } = useKakaoLogin();
-
-    useEffect(() => {
-        const token = getCookie('access_token');
-        if (token) {
-            router.replace('/');
-        }
-    });
 
     return <BottomButton onClick={kakaoLogin}>카카오 로그인하기</BottomButton>;
 }
