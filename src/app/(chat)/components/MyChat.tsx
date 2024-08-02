f6cb9e09e22eef65ac2e8490c694fd947b7725cc
jindonyy <rsefa94@gmail.com>
2024-08-02 21:38:38 +0900

    fix: (임시) 모바일 사파리에서 복사 안되는 에러 수정

--- src/app/(chat)/components/MyChat.tsx
+++ src/app/(chat)/components/MyChat.tsx
@@ -5,6 +5,7 @@ import { useLongClick } from '@/hooks/useLongClick';
 import { Flex, Stack, Text } from '@chakra-ui/react';
 import { useEffect, useRef } from 'react';
 import { useToast } from '@chakra-ui/react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 interface Props {
     type: string;
@@ -18,7 +19,7 @@ export default function MyChat(props: Props) {
     const toast = useToast({ containerStyle: { background: '#8877F5' } });
 
     const handleCopy = async () => {
-        await copy(text);
+        // await copy(text);
         toast({ description: '복사되었습니다' });
     };
 
@@ -29,18 +30,20 @@ export default function MyChat(props: Props) {
     }, [ref.current]);
 
     return (
-        <Flex ref={ref} justify="flex-end" gap="0px" fontSize="14px" lineHeight="21px" userSelect="none">
-            <Stack maxW="80%" gap="4px">
-                <Text textAlign="right">{type}</Text>
-                <Text
-                    dangerouslySetInnerHTML={{ __html: text }}
-                    p="16px"
-                    border="1px solid #E5E5E5"
-                    rounded="24px 2px 24px 24px"
-                    bg="white"
-                    boxShadow="0px 4px 12px rgba(0, 0, 0, 0.06)"
-                />
-            </Stack>
-        </Flex>
+        <CopyToClipboard text={text}>
+            <Flex ref={ref} justify="flex-end" gap="0px" fontSize="14px" lineHeight="21px" userSelect="none">
+                <Stack maxW="80%" gap="4px">
+                    <Text textAlign="right">{type}</Text>
+                    <Text
+                        dangerouslySetInnerHTML={{ __html: text }}
+                        p="16px"
+                        border="1px solid #E5E5E5"
+                        rounded="24px 2px 24px 24px"
+                        bg="white"
+                        boxShadow="0px 4px 12px rgba(0, 0, 0, 0.06)"
+                    />
+                </Stack>
+            </Flex>
+        </CopyToClipboard>
     );
 }
