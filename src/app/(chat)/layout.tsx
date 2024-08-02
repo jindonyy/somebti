e2e1facad3f7b0d1a58b36b037a2396fa70ee5f4
jindonyy <rsefa94@gmail.com>
2024-08-02 17:26:53 +0900

    feat: 모바일에서 텍스트창 focus 됐을 시 nav바는 올라가지 않도록 수정

--- src/app/(chat)/layout.tsx
+++ src/app/(chat)/layout.tsx
@@ -8,7 +8,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
     return (
         <>
             <ChatHeader />
-            <Box as="main" position="relative" zIndex="0">
+            <Box as="main" display="flex" flexDirection="column" h="100dvh" position="relative" zIndex="0">
                 {children}
                 <Navigation />
             </Box>
